13efd563e210e657a6c214264fe31f425067ddf6
Tim Riffe <tim.riffe@gmail.com>
2017-07-21 19:17:11 +0200

    added several functions to reproduce CENSUR~1, CENSUR~2, and CENSUR~3 Excel spreadsheets. Currently debugging. These all return the median ex error. Likely dozens of variants of this class of method.

--- DemoTools/dev/CENSUR.R
+++ DemoTools/dev/CENSUR.R
@@ -3,15 +3,37 @@
 ###############################################################################
 
 # based on Feeney's spreadsheet CENSUR~1.XLS
-pop1 <- c(3831870,4502304,5397061,4630775,4193184,4114704,3770907,3274822,
+pop1960 <- c(3831870,4502304,5397061,4630775,4193184,4114704,3770907,3274822,
 2744786,2559755,2160716,1839025,1494043,1133409,870238,577972,313781,131547)
-pop2 <- c(4292503,3988292,3852101,4492096,5347327,4571868,4190340,4085338,
+pop1965 <- c(3983902,3854281,4513237,5373547,4572392,4206801,4110076,3751030,
+3231736,2697217,2485095,2071540,1719370,1343444,955567,644043,341170,176068)
+pop1970 <- c(4292503,3988292,3852101,4492096,5347327,4571868,4190340,4085338,
 3674127,3198934,2648360,2382691,1970485,1584699,1172155,736258,408191,53116)
 Age  <- seq(0,85,by=5)
-date1 <- "1960-10-01"
-date2 <- "1970-10-01"
+date1960 <- "1960-10-01"
+date1965 <- "1965-10-01"
+date1970 <- "1970-10-01"
+date1960fake <- "1960-08-01"
+ex1  <- c(NA,69.45,64.62,59.72,54.87,50.11,45.37,40.65,35.99,
+31.40,26.94,22.64,18.52,14.67,11.20,8.25,5.95,6.00,NA)
+ex2  <- c(NA,70.19,65.33,60.41,55.54,50.74,45.96,41.21,
+36.52,31.89,27.39,23.05,18.89,14.99,11.45,8.43,6.06,NA)
 
-survRatio <- function(pop1, pop2, Age, date1, date2, exprior){
+# reproduces CENSUR~1.XLS
+survRatioError(pop1960,pop1970,Age,date1960,date1970,ex1)
+
+# start by debugging this
+survRatioError(
+		pop1 = pop1960,
+		pop2 = pop1965,
+		Age = Age,
+		date1 = date1960,
+		date2 = date1965,
+		exprior = ex2)
+
+
+
+survRatioError <- function(pop1, pop2, Age, date1, date2, exprior){
 	stopifnot(all.equal(length(pop1),length(pop2),length(Age)))
 	
 	# census spacing used to stagger ages.
@@ -25,7 +47,7 @@ survRatio <- function(pop1, pop2, Age, date1, date2, exprior){
 	pop1    <- pop1[-N]
 	pop2    <- pop2[-N]
 	Age     <- Age[-N]
-	
+	N       <- N - 1
 	# this 
 	if (abs(int - 5) < .01){
 		# are we super close to 5 years apart?
@@ -40,8 +62,26 @@ survRatio <- function(pop1, pop2, Age, date1, date2, exprior){
 	  }
     }
 	# all remaining columns should be identical.
-
+    
+	# Lx approximated as avg of lx
+	Lx         <- c(NA, as.vector(ma(lx, 2)))
+	# 2.5 times the pairwise sums of that to get the 5-year Lx approximation
+	Lx5        <- c(2.5 * (Lx[-length(Lx)] + Lx[-1]))
+	Lx5[N]     <- Lx[N] * exprior[N]
 	
+	# cut to size
+	Lx         <- Lx[1:N]
+	Lx5        <- Lx5[1:N]
+	exprior    <- exprior[1:N]
+	
+	Tx         <- rev(cumsum(rev(Lx5)))
+	ex.est     <- Tx / Lx
+	dif        <- exprior - ex.est
+	abserror   <- 100 * abs(dif / exprior)
+	abserror[N] <- median(abserror[-N], na.rm = TRUE)
+	
+	# return the median absolute percent error
+	median(abserror, na.rm = TRUE)
 }
 
 #' estimate survival curve from censuses spaced 5 years apart
@@ -67,8 +107,9 @@ survRatio <- function(pop1, pop2, Age, date1, date2, exprior){
 #' surv10(pop1,pop2)
 
 surv10 <- function(pop1, pop2){
+	N       <- length(pop1)
 	stagger <- 2
-	ratio   <- pop2[-c(1:stagger)] / pop1[1:(N-stagger-1)]
+	ratio   <- pop2[-c(1:stagger)] / pop1[1:(N - stagger)]
 	
 	N       <- length(ratio)
 	sx1     <- 1:N %% 2 == 1
