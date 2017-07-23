3106b758428553df65cab2cd473bf236087450c9
Tim Riffe <tim.riffe@gmail.com>
2017-07-23 22:51:11 +0200

    set up script to start on LIFIT

---
+++ DemoTools/dev/LIFIT.R
@@ -0,0 +1,8 @@
+
+# Author: tim
+###############################################################################
+
+# design this based on the manual formulas. make the same function
+# work w single or grouped ages. Use same age group detection utility elsewhere too.
+
+# input args could be any lifetable argument? or strictly lx or qx?
