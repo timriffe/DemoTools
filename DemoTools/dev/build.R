3bca360678e553674fab95ff95f74188f8562f6b
Tim Riffe <tim.riffe@gmail.com>
2017-07-26 23:26:08 +0200

    move yml files to defualt locations by devtools

--- DemoTools/dev/build.R
+++ DemoTools/dev/build.R
@@ -6,6 +6,10 @@ library(devtools)
 document("/home/tim/git/DemoTools/DemoTools")
 
 load_all("/home/tim/git/DemoTools/DemoTools")
-devtools::use_appveyor("/home/tim/git/DemoTools/DemoTools")
+use_appveyor("/home/tim/git/DemoTools/DemoTools")
+use_travis("/home/tim/git/DemoTools/DemoTools")
+
+use_coverage(pkg = "/home/tim/git/DemoTools/DemoTools", type = c("codecov", "coveralls"))
 
 check("/home/tim/git/DemoTools/DemoTools")
+?devtools::use_appveyor
