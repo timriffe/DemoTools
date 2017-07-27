b588b75a246040b61fc559c50bd2c6eef429e85a
Tim Riffe <tim.riffe@gmail.com>
2017-07-27 14:22:14 +0200

    changed some paths, remade badge files

--- dev/build.R
+++ dev/build.R
@@ -3,13 +3,12 @@
 ###############################################################################
 
 library(devtools)
-document("/home/tim/git/DemoTools/DemoTools")
+document("/home/tim/git/DemoTools")
 
-load_all("/home/tim/git/DemoTools/DemoTools")
-use_appveyor("/home/tim/git/DemoTools/DemoTools")
-use_travis("/home/tim/git/DemoTools/DemoTools")
+load_all("/home/tim/git/DemoTools")
+use_appveyor("/home/tim/git/DemoTools")
+use_travis("/home/tim/git/DemoTools")
 
-use_coverage(pkg = "/home/tim/git/DemoTools/DemoTools", type = c("codecov", "coveralls"))
+use_coverage(pkg = "/home/tim/git/DemoTools", type = c("codecov", "coveralls"))
 
-check("/home/tim/git/DemoTools/DemoTools")
-?devtools::use_appveyor
+check("/home/tim/git/DemoTools")
