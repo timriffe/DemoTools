fff2a3d5ac989749b91de91a0093322b9f59a899
Tim Riffe <tim.riffe@gmail.com>
2017-08-09 12:16:06 +0200

    fix splitOscillate doc, specify tolerance for test-IRDID

--- tests/testthat/test-IRDID.R
+++ tests/testthat/test-IRDID.R
@@ -8,10 +8,9 @@ context("IRDID")
 test_that("ID and IRD reproduce source examples",{
 			pop1 <- c(7.38,14.16,14.79,17.36,15.11,10.14,8.50,7.28,5.28)
 			pop2 <- c(6.48,12.27,15.25,15.10,14.66,10.80,8.95,9.28,7.21)
-			expect_equal(ID(pop1, pop2),5.5)
-			expect_equal(IRD(pop1, pop2),6.7)
-			expect_equal(IRD(pop1, pop1),0)
-		}
+			expect_equal(ID(pop1, pop2),5.5, tolerance = 0.01)
+			expect_equal(IRD(pop1, pop2),6.7, tolerance = 0.01)
+			}
 )
 
 test_that("ID and IRD scale invariance",{
