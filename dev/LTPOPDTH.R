f4485a1b87fd79d83260b0d1d569dc4c162f3d6a
Tim Riffe <tim.riffe@gmail.com>
2017-08-15 18:16:15 +0200

    starting roxygen for LTPOPDTH

--- dev/LTPOPDTH.R
+++ dev/LTPOPDTH.R
@@ -2,47 +2,69 @@
 # Author: tim
 ###############################################################################
 
-# pars
-sex    <- "m"
-IMR    <- 0.1 # or NA if unknown
-
-# separation factors (what is this?)
-
-code   <- 2
-# for empirical separation factors?
-Age0   <- NA   # looks like these are ax values for ages 0 and 1-4...
-Age1_4 <- NA
-
-SRB    <- 1.05 # used for both-sex lifetables only
-
-# primary inputs are pop and exposures in abridged ages. Abridged lifetables
-# used. Not a fan: they use 2.5 ax... leave arg open for axmethod, or to 
-# abridge a single age table the way the HMD does?
-# can use calcAgeAbr() plus groupAges() to move single to abridged.
-# 
-
-
 
 # 
 source("/home/tim/git/DemoTools/dev/nAx.R")
 
+#' derive nMx from nqx and nax
+#' @description This is the common ax-qx-mx identity as found in any demography text.
+#' 
+#' @param nqx numeric vector. Age specific death probabilities.
+#' @param ax numeric vector. Average time spent in interval by those dying in interval
+#' @param AgeInt integer width of each age interval
+#' 
+#' @return nMx vector of age specific death rates derived via identity.
+#' @export
+qx2mx <- function(nqx, nax, AgeInt = inferAgeIntAbr(vec = nqx)) {
+	nqx / (AgeInt - (AgeInt - nax) * nqx)      }
 
-qx2mx <- function(nqx, ax, AgeInt = inferAgeIntAbr(vec = nqx)) {
-	nqx / (AgeInt - (AgeInt - ax) * nqx)      }
-
-mx2qx <- function(mx, ax, AgeInt = inferAgeIntAbr(vec = mx)) {
-	(AgeInt * mx) / (1.0 + ((AgeInt - ax) * mx)) 
+#' derive nqx from nMx and nax
+#' @description This is the common ax-qx-mx identity as found in any demography text.
+#' 
+#' @param nMx numeric vector. Age specific death rates.
+#' @param nax numeric vector. Average time spent in interval by those dying in interval
+#' @param AgeInt integer width of each age interval
+#' 
+#' @return nqx vector of age specific death probabilities derived via identity.
+#' @export
+mx2qx <- function(nMx, nax, AgeInt = inferAgeIntAbr(vec = nMx)) {
+	(AgeInt * nMx) / (1 + (AgeInt - nax) * nMx)
 }
 
-qxmx2ax <- function(nqx, nmx, AgeInt){
-	1 / nmx - AgeInt / nqx + AgeInt
+#' derive nax from nqx and nMx
+#' @description This is the common ax-qx-mx identity as found in any demography text.
+#' 
+#' @param nMx numeric vector. Age specific death rates.
+#' @param nax numeric vector. Average time spent in interval by those dying in interval
+#' @param AgeInt integer width of each age interval
+#' 
+#' @return nax numeric vector of average time spent in interval by those dying in interval
+#' @export
+qxmx2ax <- function(nqx, nMx, AgeInt){
+	1 / nMx - AgeInt / nqx + AgeInt
 }
 
-mxax2qx <- function(nax, nMx, AgeInt, closeout = TRUE, IMR){
+#' derive nqx from nMx and nax
+#' @description This is the common ax-qx-mx identity as found in any demography text. 
+#' This is a more full-service wrapper of \code{mx2qx()}, with closeoput options and optional age 0 
+#' treatment.
+#' 
+#' @param nMx numeric vector. Age specific death rates.
+#' @param nax numeric vector. Average time spent in interval by those dying in interval
+#' @param AgeInt integer width of each age interval
+#' @param closeout logical (default \code{TRUE}). Set to 1 if TRUE. Identity otherwise
+#' @param IMR numeric. Optional q0 to impute, in case available separately.
+#' 
+#' @return nqx vector of age specific death probabilities derived via identity.
+#' @export
+mxax2qx <- function(nMx, nax, AgeInt, closeout = TRUE, IMR){
 	
-	qx <- (AgeInt * nMx) / (1 + (AgeInt - nax) * nMx)
+	qx <- mx2qx(nMx, nax, AgeInt)
 	if (closeout){
 		qx[length(qx)] <- 1
+		if (length(nMx) == 1){
+			warning("only a single nMx was given, and it was treated as age omega, with qx = 1.\nSpecify closeout = FALSE otherwise")
+		}
 	}
 	if (!missing(IMR) & !is.na(IMR)){
 		qx[1] <- IMR
@@ -51,7 +73,7 @@ mxax2qx <- function(nax, nMx, AgeInt, closeout = TRUE, IMR){
 }
 
 
-qx2lx <- function(nqx,radix=1e5){
+qx2lx <- function(nqx, radix=1e5){
 	
 	radix * cumprod(c(1, 1 - nqx[-length(nqx)]))
 }
@@ -68,15 +90,68 @@ lxdxax2Lx <- function(lx,dx,ax,AgeInt){
 	Lx
 }
 Lx2Tx <- function(Lx){
-	rev(cumsum(rev(nLx)))
+	rev(cumsum(rev(Lx)))
 }
 
 
 
 
-# some calcs
-
-
+#' @examples
+#' # trial code from PAS LTPOPDTH, North, Males, IMR = .1
+#' Exposures <- c(100958,466275,624134,559559,446736,370653,301862,249409,
+#' 		247473,223014,172260,149338,127242,105715,79614,53660,
+#' 		31021,16805,8000,4000,2000,1000)
+#' 
+#' Deaths <- c(8674,1592,618,411,755,1098,1100,1357,
+#' 		1335,3257,2200,4023,2167,4578,2956,4212,
+#' 		2887,2351,1500,900,500,300)
+#' # lower age bounds
+#' Age    <- c(0, 1, seq(5, 100, by = 5))
+#' AgeInt <- c(diff(Age), NA)
+#' 
+#' 
+#' 
+#' PASLT <- LTabr(Deaths = Deaths, 
+#' 		Exposures = Exposures, 
+#' 		Age = Age,
+#' 		AgeInt =AgeInt,
+#' 		axmethod = "PAS",
+#' 		IMR = .1,
+#' 		region = "N",
+#' 		Sex = "M")
+#' # de facto unit test. The unsmoothed output from PAS
+#' # spreadsheet (allow for rounding error in last decimal)
+#' excheck <- c(56.31,61.53,58.35,53.63, 
+#' 		48.81,44.21,39.83,35.52,
+#' 		31.43,27.22,24.09,20.52,
+#' 		18.12,14.51,12.42,9.45,7.85, 
+#' 		6.09,4.95,4.28,3.85,3.33)
+#' stopifnot(max(abs(round(PASLT$ex,2) - excheck)) <= .01)
+#' # examples based on UN 1982 (p. 34)
+#' Mx <- c(.23669,.04672,.00982,.00511,.00697,.01036,.01169,
+#' 		.01332,.01528,.01757,.02092,.02517,.03225,.04241,.06056,
+#' 		.08574,.11840,.16226,.23745)
+#' excheckUN <-  c(35.000,42.901,47.190,44.438,
+#' 		40.523,36.868,33.691,30.567,27.500,24.485,21.504,18.599,
+#' 		15.758,13.080,10.584,8.466,6.729,5.312,4.211)
+#' AgeInt <- inferAgeIntAbr(vec = Mx)
+#' 
+#' 
+#' UNLT1 <- LTabr(nMx = Mx,
+#' 		Age = c(0,1,seq(5,85,by=5)),
+#' 		AgeInt = AgeInt,
+#' 		axmethod = "UN",
+#' 		Sex = "M", 
+#' 		kludge = FALSE)
+#' UNLT2 <- LTabr(nMx = Mx,
+#' 		Age = c(0,1,seq(5,85,by=5)),
+#' 		AgeInt = AgeInt,
+#' 		axmethod = "UN",
+#' 		Sex = "M", 
+#' 		kludge = TRUE)
+#' # TODO: possibly there is more intermediate rounding happening in the FOR code?
+#' round(UNLT2$ex,2) - round(excheckUN,2)
+#' UNLT2$ex - UNLT1$ex
 LTabr <- function(
 		Deaths, 
 		Exposures, 
@@ -115,8 +190,7 @@ LTabr <- function(
 				      AgeInt = AgeInt, 
 				      IMR = IMR, 
 				      Sex = Sex, 
-				      region = region,
-					  SRB = SRB)
+				      region = region)
       }
 	if (axmethod == "un"){
 		# UN method just CD west for now, so no region arg
@@ -127,7 +201,7 @@ LTabr <- function(
 					  kludge = kludge)
 	}
     # these are the same always. Only thing different is ax method.
-	nqx <- mxax2qx(nax = nAx, nMx = nMx, AgeInt = AgeInt, closeout = TRUE, IMR = IMR)
+	nqx <- mxax2qx(nMx = nMx, nax = nAx, AgeInt = AgeInt, closeout = TRUE, IMR = IMR)
 	# TODO: differences might begin here
 	lx  <- qx2lx(nqx)
 	ndx <- lx2dx(lx)
@@ -153,36 +227,7 @@ LTabr <- function(
 
 
 
-# trial code from PAS LTPOPDTH, North, Males, IMR = .1
-Exposures <- c(100958,466275,624134,559559,446736,370653,301862,249409,
-		247473,223014,172260,149338,127242,105715,79614,53660,
-		31021,16805,8000,4000,2000,1000)
 
-Deaths <- c(8674,1592,618,411,755,1098,1100,1357,
-		1335,3257,2200,4023,2167,4578,2956,4212,
-		2887,2351,1500,900,500,300)
-# lower age bounds
-Age    <- c(0, 1, seq(5, 100, by = 5))
-AgeInt <- c(diff(Age), NA)
-
-
-
-PASLT <- LTabr(Deaths = Deaths, 
-		Exposures = Exposures, 
-		Age = Age,
-		AgeInt =AgeInt,
-		axmethod = "PAS",
-		IMR = .1,
-		region = "N",
-		Sex = "M")
-# de facto unit test. The unsmoothed output from PAS
-# spreadsheet (allow for rounding error in last decimal)
-excheck <- c(56.31,61.53,58.35,53.63, 
-		48.81,44.21,39.83,35.52,
-		31.43,27.22,24.09,20.52,
-		18.12,14.51,12.42,9.45,7.85, 
-		6.09,4.95,4.28,3.85,3.33)
-stopifnot(max(abs(round(PASLT$ex,2) - excheck)) <= .01)
 
 # example data from United Nations Model Life Tables -
 # Males Latin American Pattern
@@ -190,34 +235,21 @@ stopifnot(max(abs(round(PASLT$ex,2) - excheck)) <= .01)
 Mx <- c(.23669,.04672,.00982,.00511,.00697,.01036,.01169,
 .01332,.01528,.01757,.02092,.02517,.03225,.04241,.06056,
 .08574,.11840,.16226,.23745)
-ax <- c(0.330,1.352,2.500,2.500,2.633,2.586,2.528,2.528,
- 2.526,2.529,2.531,2.538,2.542,2.543,2.520,2.461,2.386,2.295,4.211)
 excheckUN <-  c(35.000,42.901,47.190,44.438,
 40.523,36.868,33.691,30.567,27.500,24.485,21.504,18.599,
 15.758,13.080,10.584,8.466,6.729,5.312,4.211)
-mxax2qx(ax,Mx,AgeInt,IMR=NA)
+AgeInt <- inferAgeIntAbr(vec = Mx)
 
-length(inferAgeIntAbr(vec = Mx))
-nAx1       <- axUN(nMx = Mx, 
-		          AgeInt = inferAgeIntAbr(vec = Mx), 
-		          Sex = "m",
-				  kludge = FALSE)
-nAx2       <- axUN(nMx = Mx, 
-				  AgeInt = inferAgeIntAbr(vec = Mx), 
-				  Sex = "m",
-				  kludge = TRUE)	  
-# this is acceptable...
-round(nAx1,3) - ax
-round(nAx2,3) - ax
+
 UNLT1 <- LTabr(nMx = Mx,
 		Age = c(0,1,seq(5,85,by=5)),
-		AgeInt = inferAgeIntAbr(vec = Mx),
+		AgeInt = AgeInt,
 		axmethod = "UN",
 		Sex = "M", 
 		kludge = FALSE)
 UNLT2 <- LTabr(nMx = Mx,
 		Age = c(0,1,seq(5,85,by=5)),
-		AgeInt = inferAgeIntAbr(vec = Mx),
+		AgeInt = AgeInt,
 		axmethod = "UN",
 		Sex = "M", 
 		kludge = TRUE)
@@ -240,16 +272,14 @@ qx2mx(0.16326, .35, 1)
 mx2qx(0.16326, .35, 1)
 
 # a MORTPACK unit test:
-MPnMx <- c(0.12846,0.02477,0.00603,0.0034,
-0.00417,0.00513,0.00581,0.00645,0.00725,
-0.00813,0.00913,0.01199,0.01647,
-0.0256,0.04047,0.06624,0.10638,0.19611)
-
-
-
-UNLT2 <- LTabr(
-		nMx = MPnMx,
-		Age = c(0,1,seq(5,80,by=5)),
-		AgeInt = inferAgeIntAbr(vec = MPnMx),
-		axmethod = "UN",
-		Sex = "F")
+#MPnMx <- c(0.12846,0.02477,0.00603,0.0034,
+#0.00417,0.00513,0.00581,0.00645,0.00725,
+#0.00813,0.00913,0.01199,0.01647,
+#0.0256,0.04047,0.06624,0.10638,0.19611)
+#
+#UNLT2 <- LTabr(
+#		nMx = MPnMx,
+#		Age = c(0,1,seq(5,80,by=5)),
+#		AgeInt = inferAgeIntAbr(vec = MPnMx),
+#		axmethod = "UN",
+#		Sex = "F")
