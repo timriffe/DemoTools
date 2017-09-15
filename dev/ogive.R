625f38c75be7ca7f401945bb19d6b6b4d464a750
Tim Riffe <tim.riffe@gmail.com>
2017-09-15 08:09:56 +0100

    blank dev script for ogive, just notes for now

---
+++ dev/ogive.R
@@ -0,0 +1,12 @@
+
+# Author: tim
+###############################################################################
+
+# notes from patrick:
+# should be a generic function with loess as first option under the hood,
+# choice between user-specified smoothing paramater and gcv crit
+# 2nd option would be 2nd degree polynomial (no auto detect, but in general
+# intended when there are few points, like fert in 5-yr age groups), but don't sweat too much
+# about endpoints as there are purpose-designed methods for particular 
+# phenomena.
+
