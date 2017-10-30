6fa00bba0c746099e0997fa6af635f99cb24e2c1
Tim Riffe <tim.riffe@gmail.com>
2017-10-30 17:07:42 +0100

    adapted greg's fix, thanks! added unit test to examples for cheap auto checking redocumented, upped version nr

--- R/AGEHEAP.R
+++ R/AGEHEAP.R
@@ -21,37 +21,42 @@
 
 #' @export 
 #' @examples 
-#' Value <- c(80626,95823,104315,115813,100796,105086,97266,116328,
-#' 		75984,89982,95525,56973,78767,65672,53438,85014,
-#' 		47600,64363,42195,42262,73221,30080,34391,29072,
-#' 		20531,66171,24029,44227,24128,23599,82088,16454,
-#' 		22628,17108,12531,57325,17220,28425,16206,17532,
-#' 		65976,11593,15828,13541,8133,44696,11165,18543,
-#' 		12614,12041,55798,9324,10772,10453,6773,28358,
-#' 		9916,13348,8039,7583,42470,5288,5317,6582,
-#' 		3361,17949,3650,5873,3279,3336,27368,1965,
-#' 		2495,2319,1335,12022,1401,1668,1360,1185,
-#' 		9167,424,568,462,282,6206,343,409,333,291,4137,133,169,157,89,2068,68,81,66,57)
-#' Age <- 0:99
-#' 
-#' Whipple(Value, Age, 25, 60, digit = c(0,5)) # 2.34,replicates SINGAGE males
-#' 
-#' # implements formula from GDA_1981_Structures-par-age-et-sexe-en-Afrique_[IREDA]
-#' # p 148
-#' Whipple(Value, Age, 25, 60, digit = 0)
-#' Whipple(Value, Age, 25, 60, digit = 5) 
-#' 
-#' # Whipple types
-#' Whipple(Value, Age, 25, 60, digit = 3) 
-
+#'  Value <- c(80626,95823,104315,115813,100796,105086,97266,116328,
+#'  		75984,89982,95525,56973,78767,65672,53438,85014,
+#'  		47600,64363,42195,42262,73221,30080,34391,29072,
+#'  		20531,66171,24029,44227,24128,23599,82088,16454,
+#'  		22628,17108,12531,57325,17220,28425,16206,17532,
+#'  		65976,11593,15828,13541,8133,44696,11165,18543,
+#'  		12614,12041,55798,9324,10772,10453,6773,28358,
+#'  		9916,13348,8039,7583,42470,5288,5317,6582,
+#'  		3361,17949,3650,5873,3279,3336,27368,1965,
+#'  		2495,2319,1335,12022,1401,1668,1360,1185,
+#'  		9167,424,568,462,282,6206,343,409,333,291,4137,133,169,157,89,2068,68,81,66,57)
+#'  Age <- 0:99
+#'  
+#'  (w05 <- Whipple(Value, Age, 25, 60, digit = c(0,5))) # 2.34,replicates SINGAGE males
+#'  
+#'  # implements formula from GDA_1981_Structures-par-age-et-sexe-en-Afrique_[IREDA]
+#'  # p 148
+#'  (w0 <- Whipple(Value, Age, 25, 60, digit = 0))
+#'  (w5 <- Whipple(Value, Age, 25, 60, digit = 5)) 
+#'  
+#'  # de facto unit test:
+#'  stopifnot(round(w05,2) == 2.34)
+#'  
+#'  # Whipple types
+#'  Whipple(Value, Age, 25, 60, digit = 3) 
 Whipple <- function(Value, Age, ageMin = 25, ageMax = 65, digit = c(0,5)){
-	stopifnot(length(digit) == 1 || identical(digit, c(0, 5)))
+	
+	# TR: changed to all %in% since identical not order independent.
+	stopifnot(length(digit) == 1 || all(digit %in% c(0, 5)))
 	stopifnot(length(Value) == length(Age))
 	
 	numeratorind   <- Age >= ageMin & Age <= ageMax & Age %% 10 %in% digit
 	# if we are checking just one digit, go down 7 up two, so that the right nr
 	# of counts in denom. This per the French formulas.
-	denominatorind <- Age >= (ageMin - ifelse(identical(digit, c(0,5)),2,7)) & Age <= (ageMax + 2)
+	# TR: sufficient to check length here, since hard check done above
+	denominatorind <- Age >= (ageMin - ifelse(length(digit) == 2,2,7)) & Age <= (ageMax + 2)
 	
 	whip           <- ifelse(length(digit) == 2,5,10) * sum(Value[numeratorind]) / sum(Value[denominatorind])
 	
