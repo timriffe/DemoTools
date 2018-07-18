ed2d38412444f213fd5e94b7aadb904a37e1fa5d
Tim Riffe <tim.riffe@gmail.com>
2018-07-18 16:52:34 +0200

    AGESMTH.R indentation, removed rounding. Need to modularize

--- dev/AGESMTH.R
+++ dev/AGESMTH.R
@@ -1,4 +1,5 @@
 # Author: sean
+# modified by TR 18 July, 2018
 ###############################################################################
 #
 #MalePop      <- c(642367, 515520, 357831, 275542, 268336, 278601, 242515, 198231, 165937, 122756, 96775, 59307, 63467, 32377, 29796, 16183, 34729)
@@ -20,11 +21,14 @@
 #' @export
 #' 
 #' @examples 
-#' MalePop      <- c(642367, 515520, 357831, 275542, 268336, 278601, 242515, 198231, 165937, 122756, 96775, 59307, 63467, 32377, 29796, 16183, 34729)
-#' FemalePop    <- c(654258, 503070, 323460, 265534, 322576, 306329, 245883, 179182, 145572, 95590, 81715, 48412, 54572, 28581, 26733, 14778, 30300)
-#' underOnePop <- c(321476, 332782, 503070, 323460, 265534, 322576, 306329, 245883, 179182, 145572, 95590, 81715, 48412, 54572, 28581, 26733, 14778, 30300)
+#' MalePop      <- c(642367, 515520, 357831, 275542, 268336, 278601, 242515, 
+#'   198231, 165937, 122756, 96775, 59307, 63467, 32377, 29796, 16183, 34729)
+#' FemalePop    <- c(654258, 503070, 323460, 265534, 322576, 306329, 245883, 
+#'   179182, 145572, 95590, 81715, 48412, 54572, 28581, 26733, 14778, 30300)
+#' underOnePop  <- c(321476, 332782, 503070, 323460, 265534, 322576, 306329, 
+#'   245883, 179182, 145572, 95590, 81715, 48412, 54572, 28581, 26733, 14778, 30300)
 #' Ages <- seq(0, 80, by = 5)
-#' Ages2 <- c(0,1,5,10,15,20,25,30,35,40,45,50,55,60,65,70,75,80)
+#' Ages2 <- c(0,1,seq(5, 80, by = 5))
 #' 
 #' popAgeSmth(MalePop, Ages, "Carrier-Farrag")
 #' popAgeSmth(FemalePop, Ages, "KKN")
@@ -32,7 +36,11 @@
 #' popAgeSmth(MalePop, Ages, "UN", SplitU5 = FALSE)
 #' popAgeSmth(underOnePop, Ages2, Method="Strong", SplitU5 = TRUE)
 
-popAgeSmth <- function(Value, Age, Method, SplitU5 = FALSE){
+popAgeSmth <- function(
+		Value, 
+		Age, 
+		Method, 
+		SplitU5 = FALSE){
   if (SplitU5){
     intermediateAgg <- convertSplitTo5Year(Value) #Consolidate under split under 5 group
     newAges <- seq(0, max(Age), by=5) #Create new age groups
@@ -46,78 +54,79 @@ popAgeSmth <- function(Value, Age, Method, SplitU5 = FALSE){
   # Aggregate groups and create method inputs
   if (Method == "Carrier-Farrag" || Method == "KKN" || Method == "Arriaga" || Method == "Strong"){
     Value10PxMinus10 <- groupAges(aggValue, Age = newAges, N = 10)
-    Value10Px <- Value10PxMinus10[-1]
-    Value10PxPlus10 <- Value10Px[-1]
+    Value10Px        <- Value10PxMinus10[-1]
+    Value10PxPlus10  <- Value10Px[-1]
     
-    Value10PxPlus10 <- Value10PxPlus10[0:(length(Value10PxPlus10)-1)]
-    Value10Px <- Value10Px[0:(length(Value10Px)-2)]
+    Value10PxPlus10  <- Value10PxPlus10[0:(length(Value10PxPlus10)-1)]
+    Value10Px        <- Value10Px[0:(length(Value10Px)-2)]
     Value10PxMinus10 <- Value10PxMinus10[0:(length(Value10PxMinus10)-3)]
   }
   else if (Method == "UN"){
-    Value5PxMinus10 <- groupAges(aggValue, Age = newAges, N = 5) #aggregate to 5 year groups
-    Value5PxMinus5 <- Value5PxMinus10[-1]
-    Value5Px <- Value5PxMinus5[-1]
-    Value5PxPlus5 <- Value5Px[-1]
-    Value5PxPlus10 <- Value5PxPlus5[-1]
+    Value5PxMinus10  <- groupAges(aggValue, Age = newAges, N = 5) #aggregate to 5 year groups
+    Value5PxMinus5   <- Value5PxMinus10[-1]
+    Value5Px         <- Value5PxMinus5[-1]
+    Value5PxPlus5    <- Value5Px[-1]
+    Value5PxPlus10   <- Value5PxPlus5[-1]
     
-    Value5PxPlus10 <- Value5PxPlus10[0:(length(Value5PxPlus10)-1)]
-    Value5PxPlus5 <- Value5PxPlus5[0:(length(Value5PxPlus5)-2)]
-    Value5Px <- Value5Px[0:(length(Value5Px)-3)]
-    Value5PxMinus5 <- Value5PxMinus5[0:(length(Value5PxMinus5)-4)]
-    Value5PxMinus10 <- Value5PxMinus10[0:(length(Value5PxMinus10)-5)]
+    Value5PxPlus10   <- Value5PxPlus10[0:(length(Value5PxPlus10)-1)]
+    Value5PxPlus5    <- Value5PxPlus5[0:(length(Value5PxPlus5)-2)]
+    Value5Px         <- Value5Px[0:(length(Value5Px)-3)]
+    Value5PxMinus5   <- Value5PxMinus5[0:(length(Value5PxMinus5)-4)]
+    Value5PxMinus10  <- Value5PxMinus10[0:(length(Value5PxMinus10)-5)]
   }
   
   # Apply method-specific formula
   if (Method == "Carrier-Farrag"){
-    Value5PxPlus5 <- Value10Px / (1 + (Value10PxMinus10 / Value10PxPlus10)^(1/4))
-    Value5Px <- Value10Px - Value5PxPlus5
+    Value5PxPlus5    <- Value10Px / (1 + (Value10PxMinus10 / Value10PxPlus10)^(1/4))
+    Value5Px         <- Value10Px - Value5PxPlus5
   }
   else if (Method == "KKN"){
-    Value5Px <- (1/2)*Value10Px + (1/16)*(Value10PxMinus10 - Value10PxPlus10)
-    Value5PxPlus5 <- Value10Px - Value5Px
+    Value5Px         <- (1/2) * Value10Px + (1/16)*(Value10PxMinus10 - Value10PxPlus10)
+    Value5PxPlus5    <- Value10Px - Value5Px
   }
   else if (Method == "Arriaga"){
-    Value5PxPlus5 <- (-Value10PxMinus10 + 11*Value10Px + 2*Value10PxPlus10)/24
-    Value5Px <- Value10Px - Value5PxPlus5
+    Value5PxPlus5    <- (-Value10PxMinus10 + 11*Value10Px + 2*Value10PxPlus10)/24
+    Value5Px         <- Value10Px - Value5PxPlus5
   }
   else if (Method == "UN"){
-    Value5PxPrime <- (1/16)*(-Value5PxMinus10 + 4*Value5PxMinus5 + 10*Value5Px + 4*Value5PxPlus5 - Value5PxMinus10)
+    Value5PxPrime    <- (1/16 )* (-Value5PxMinus10 + 4*Value5PxMinus5 + 10*Value5Px + 
+				                  4*Value5PxPlus5 - Value5PxMinus10)
   }
   else if (Method == "Strong"){
-    Value10PxPrime <- (Value10PxMinus10 + 2*Value10Px + Value10PxPlus10)/4
+    Value10PxPrime   <- (Value10PxMinus10 + 2*Value10Px + Value10PxPlus10)/4
   }
   
   #Create output
   if (Method == "Carrier-Farrag" || Method == "KKN" || Method == "Arriaga"){
     #Combine sequences and create full 5-year age group structure
-    SmthPop <- rep(0, length(aggValue)/5)
-    SmthPop[seq(3, length(SmthPop) - 4, by=2)] <- round(Value5Px)
-    SmthPop[seq(4, length(SmthPop) - 3, by=2)] <- round(Value5PxPlus5)
+    SmthPop                                    <- rep(0, length(aggValue)/5)
+    SmthPop[seq(3, length(SmthPop) - 4, by=2)] <- Value5Px
+    SmthPop[seq(4, length(SmthPop) - 3, by=2)] <- Value5PxPlus5
     
     #Add on the under 10 and final age groups
-    Value5YearGroups <- groupAges(aggValue, Age = newAges)
-    SmthPop[seq(1, 2)] <- Value5YearGroups[1:2]
+    Value5YearGroups                           <- groupAges(aggValue, Age = newAges)
+    SmthPop[seq(1, 2)]                         <- Value5YearGroups[1:2]
     SmthPop[seq(length(SmthPop)-2, length(SmthPop))] <- Value5YearGroups[(length(Value5YearGroups)-2):length(Value5YearGroups)]
   }
   else if (Method == "UN"){
     #Combine sequences and create full 5-year age group structure
-    SmthPop <- rep(0, length(aggValue)/5)
-    SmthPop[seq(3, length(SmthPop) - 3)] <- round(Value5PxPrime)
+    SmthPop                                    <- rep(0, length(aggValue)/5)
+    SmthPop[seq(3, length(SmthPop) - 3)]       <- Value5PxPrime
     
     #Add on the under 10 and final age groups
-    Value5YearGroups <- groupAges(aggValue, Age = newAges)
-    SmthPop[seq(1, 2)] <- Value5YearGroups[1:2]
+    Value5YearGroups                           <- groupAges(aggValue, Age = newAges)
+    SmthPop[seq(1, 2)]                         <- Value5YearGroups[1:2]
     SmthPop[seq(length(SmthPop)-2, length(SmthPop))] <- Value5YearGroups[(length(Value5YearGroups)-2):length(Value5YearGroups)]
   }
   else if (Method == "Strong"){
     #Combine sequences and create full 5-year age group structure
-    SmthPop <- rep(0, length(aggValue)/5)
-    SmthPop[seq(3, length(SmthPop) - 4, by=2)] <- round(Value10PxPrime/2)
-    SmthPop[seq(4, length(SmthPop) - 3, by=2)] <- round(Value10PxPrime/2)
+    SmthPop                                    <- rep(0, length(aggValue)/5)
+    SmthPop[seq(3, length(SmthPop) - 4, by=2)] <- Value10PxPrime/2
+    SmthPop[seq(4, length(SmthPop) - 3, by=2)] <- Value10PxPrime/2
     
     #Add on the under 10 and final age groups
-    Value5YearGroups <- groupAges(aggValue, Age = newAges)
-    SmthPop[seq(1, 2)] <- Value5YearGroups[1:2]
+    Value5YearGroups                           <- groupAges(aggValue, Age = newAges)
+    SmthPop[seq(1, 2)]                         <- Value5YearGroups[1:2]
     SmthPop[seq(length(SmthPop)-2, length(SmthPop))] <- Value5YearGroups[(length(Value5YearGroups)-2):length(Value5YearGroups)]
   }
   
