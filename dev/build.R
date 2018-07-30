52b6b6a43c27fe04f4f6d2e4c2922544038d0c7b
Tim Riffe <tim.riffe@gmail.com>
2018-07-30 22:46:12 +0200

    update readme

--- dev/build.R
+++ dev/build.R
@@ -11,7 +11,7 @@ library(devtools)
 #install_github("hadley/devtools")
 # do this whenever new functions are added to /R, or whenever roxygen is updated
 devtools::document("/home/tim/git/DemoTools")
-calcAgeAbr
+
 # run this to get access to already-written functions
 shhh(load_all("/home/tim/git/DemoTools"))
 
