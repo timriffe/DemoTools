c10d3468cd81562f9b5ea4cfab441f0f29029cfa
Tim Riffe <tim.riffe@gmail.com>
2018-08-16 16:24:33 +0200

    add package help file, to be expanded

---
+++ R/DemoTools-package.R
@@ -0,0 +1,16 @@
+#' DemoTools: Standardize, Evaluate, and Adjust Demographic Data
+#' 
+#' @import demogR Rdpack
+#' @author \itemize{
+#' \item {Tim Riffe}
+#' \item {Jose Manuel Aburto}
+#' \item {Sean Fennell}
+#' \item {Marius D. Pascariu}
+#' }
+#' 
+#' Maintainer: Tim Riffe  <tim.riffe@@gmail.com>
+#' @name DemoTools-package
+#' @docType package
+NULL
+
+# examples forthcoming
