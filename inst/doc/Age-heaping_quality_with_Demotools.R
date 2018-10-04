866ee9a86a5de021f77fcf630562112975abe422
Tim Riffe <tim.riffe@gmail.com>
2018-10-04 13:05:51 +0200

    add vignette building to the build.R script

---
+++ inst/doc/Age-heaping_quality_with_Demotools.R
@@ -0,0 +1,8 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  tidy = FALSE,
+  comment = "#>",
+  fig.width = 6, fig.height = 6
+)
+
