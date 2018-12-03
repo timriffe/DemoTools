4fb99cff3df65b90f15b3fbcbb4256cc771f3fd8
Tim Riffe <tim.riffe@gmail.com>
2018-12-03 15:25:30 +0100

    rebuild vignette on age heaping, add to inst/doc

---
+++ inst/doc/Age-heaping_quality_with_Demotools.R
@@ -0,0 +1,43 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  tidy = FALSE,
+  comment = "#>",
+  fig.width = 6, fig.height = 6
+)
+
+## ------------------------------------------------------------------------
+library(DemoTools)
+
+# 'pop1m_ind' available as package data
+Value <- pop1m_ind
+Age   <- 0:(length(Value)-1)
+  
+plot(Age, Value, type = 'l')  
+  
+
+## ------------------------------------------------------------------------
+Whipple <-  Whipple(Value, Age, ageMin = 23, ageMax = 62, digit = c(0,5))
+Whipple
+
+## ------------------------------------------------------------------------
+Myers <- Myers(Value, Age, ageMin = 10, ageMax = 90) 
+Myers
+
+
+## ------------------------------------------------------------------------
+Bachi <- Bachi(Value, Age, ageMin = 10, ageMax = 90)
+Bachi
+
+## ------------------------------------------------------------------------
+Noumbissi <- Noumbissi(Value, Age, digit = 0) 
+Noumbissi
+
+## ------------------------------------------------------------------------
+Spoorenberg <- Spoorenberg(Value, Age, ageMin = 20, ageMax = 64)
+Spoorenberg
+
+## ------------------------------------------------------------------------
+CoaleLi_index <- CoaleLi(Value, Age, ageMin = 60, ageMax = max(Age), terms = 5, digit = 0)
+CoaleLi_index
+
