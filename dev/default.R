c918723eaf62248344ce00a45e404e9b41a44581
Tim Riffe <tim.riffe@gmail.com>
2018-12-28 20:16:33 +0100

    prototype created!

--- dev/default.R
+++ dev/default.R
@@ -8,9 +8,10 @@
 # take arbitrary data in single or other age groups, and produce single age data... somehow.
 
 # 1) need opag(), with decent default standard.
+library(DemoTools)
 library(magrittr)
 pop_in <- pop1m_pasex
-Age <- 0:99
+Age    <- 0:99
 
 smoothed <- pop_in %>%
 		agesmth(Age=Age,
@@ -19,55 +20,273 @@ smoothed <- pop_in %>%
  				young.tail = "Arriaga") %>%
         sprague(OAG=FALSE) %>%
 		rescale.vector(scale=sum(pop1m_pasex))
-    
-plot(Age, smoothed, type='l')
 
 # agesmth recommended, followed by sprague
-h1 <- heapify(smoothed, Age, p0 = 1, p5 = .5, 20, 95)
+h1 <- heapify(smoothed, Age, p0 = .1, p5 = .1, 20, 95)
 # case where beers() sufficient, but sprague not enough:
-h2 <- heapify(smoothed, Age, p0 = .5, p5 = .5, 20, 95)
+h2 <- heapify(smoothed, Age, p0 = .2, p5 = .2, 20, 95)
 # go even gentler: sprague-only sufficient.
-h3 <- heapify(smoothed, Age, p0 = .2, p5 = .05, 20, 95)
-# insance 0-pref, light 5-pref
-h4 <- heapify(smoothed, Age, p0 = 1.5, p5 = .5, 20, 95)
+h3 <- heapify(smoothed, Age, p0 = .3, p5 = .2, 20, 95)
+# insane 0-pref, light 5-pref
+h4 <- heapify(smoothed, Age, p0 = .5, p5 = .3, 20, 95)
+# insane heaping, even 0-5 pref
+h5 <- heapify(smoothed, Age, p0 = .43, p5 = .5, 20, 95)
 
-# agesmth recommended
-plot(Age, smoothed, type='l',col=gray(.8),lwd=4)
-lines(Age, h1, col = gray(.5))
-lines(Age, h2, col = gray(.3))
-lines(Age, h3, col = gray(.1))
-lines(Age, h4, col = gray(.1))
-# what does Myers say?
-Myers(smoothed,Age) # .3 % would need to be redistributed
-Myers(h1,Age) # 9 % would need to be redistributed
-Myers(h2,Age) # 4% would need to be redistributed
-Myers(h3,Age) # 1/2 % would need to be redistributed
-DemoTools::Myers
-# ratio (exaggeration)
-Whipple(h1,Age) # 48 %
-Whipple(h2,Age) # 25 %
-Whipple(h3,Age) # 2.8 %
+cols <- RColorBrewer::brewer.pal(7,"Reds")
+plot(Age, smoothed, type='l',lwd=4,col=gray(.7),ylab = "counts")
+lines(Age, h1, col = cols[7])
+lines(Age, h2, col = cols[6])
+lines(Age, h3, col = cols[5])
+lines(Age, h4, col = cols[4])
+lines(Age, h5, col = cols[3])
 
+
+assess_pop <- function(Pop, Age, ageMin = 30, ageMax = min(c(70,max(Age[Age%%10 == 0])-10))){
+	
+	# single age checks only if single age data given:
+	wi <- NULL
+	mi <- NULL
+	do.single <- is_single(Age)
+	if (do.single){
+		wi <- Whipple(Value = Pop, Age = Age, ageMin = ageMin, ageMax = ageMax, digit = c(0,5))
+		mi <- Myers(Value = Pop, Age = Age, ageMin = ageMin, ageMax = ageMax)
+	}
+	# in all cases can run 5-year checks:
+	si <- as.numeric(zero_pref_sawtooth(Value = Pop, Age = Age, ageMin = ageMin, ageMax = ageMax))
+	ri <- five_year_roughness(Value = Pop, Age = Age, ageMin = ageMin, ageMax = ageMax)
+	
+	# negative lower bound to capture 0s
+	# whipple break points:
+	w_breaks <- c(-.01, 1.05, 1.2, 1.5, 10)
+# 4 levels: negligible | light | moderate | heavy
+# Myers break points:
+	m_breaks <- c(-.01, 1, 3, 10, 100)
+# 4 levels: negligible | light | moderate | heavy
+	
+# five-year-roughness breaks:
+	r_breaks <- c(-.01, .1, .5, Inf)
+# 3-level: light | mod | heavy (if not met with some sawtooth, then needs visual confirmation.)
+	
+# 0-pref-sawtooth beaks
+	s_breaks <- c(-.01,1, 1.1, 1.5, Inf) # last interval most aggressive
+# 4 levels: negligible | light | mod | heavy
+	
+	r_level <- as.character(cut(ri, r_breaks, labels = c("light", "moderate", "heavy")))
+	s_level <- as.character(cut(si, s_breaks, labels = c("negligible", "light", "moderate", "heavy")))
+	
+	if (do.single){
+		w_level <- as.character(cut(wi, w_breaks, labels = c("negligible", "light", "moderate", "heavy")))
+		m_level <- as.character(cut(mi, m_breaks, labels = c("negligible", "light", "moderate", "heavy")))
+	} else {
+		w_level <- NA
+		m_level <- NA
+	}
+
+	c(Whipple = w_level, Myers = m_level, Roughness = r_level, Sawtooth = s_level)
+}
+
+# assessment coming from assess_pop()
+plan_pop_adjustment <- function(assessment){
+	
+	levs <- c("negligible","light","moderate","heavy")
+	need_plan <- TRUE
+	# if we have single age data we either do or do not look 
+	# at the 5-year data, depending on how it is
+	if (!any(is.na(assessment[c("Whipple","Myers")]))){
+		if (any(assessment[c("Whipple","Myers")] == "negligible")){
+			the_plan  <- "as-is"
+			need_plan <- FALSE
+		}
+		if (any(assessment[c("Whipple","Myers")] == "light") &
+				assessment["Sawtooth"] == "negligible" &
+				assessment["Roughness"] == "light"){
+			the_plan  <- "sprague"
+			need_plan <- FALSE
+		}
+		# can have terrible heaping with little consequence in 5-year age groups too
+		if (any(assessment[c("Whipple","Myers")] == "heavy") &
+				assessment["Sawtooth"] == "light"){
+			the_plan  <- "agesmth(constrained)-sprague"
+			need_plan <- FALSE
+		}
+	}
+	
+	# otherwise, the rest depends on the 5-year data assessment.
+	
+	# beers is the lightest treatment
+	if (assessment["Roughness"] %in% c("light","moderate") & 
+			assessment["Sawtooth"] == "light" & need_plan){
+		    the_plan <- "beers"
+			need_plan <- FALSE
+	}
+	if (assessment["Roughness"] %in% c("light","moderate") & 
+			assessment["Sawtooth"] == "moderate" & need_plan){
+		the_plan <- "agesmth(constrained)-sprague"
+		need_plan <- FALSE
+	}
+	if ((assessment["Sawtooth"] == "heavy" | 
+					(sum(assessment == "heavy") > 2)) & need_plan){
+		the_plan <- "agesmth(aggressive)-sprague"
+		need_plan <- FALSE
+	}
+	
+
+	# for example if roughness is heavy, but sawtooth not triggered, what's going on?
+	# maybe just abrupt fertiltiy history, but worth looking at.
+	if (need_plan){
+		the_plan <- "visual assessment required"
+	}
+	
+	the_plan	
+}
+
+adjust_pop_with_plan <- function(Pop, Age, plan, OAG = TRUE){
+	
+	if (plan == "as-is"){
+		return(Pop)
+	}
+	if (plan == "sprague"){
+		out <- sprague(Pop, Age, OAG = OAG)
+		if (any(out < 0)){
+			cat("Warning: sprague produced negative values")
+		}
+		return(out)
+	}
+	if (plan == "beers"){
+		out <- beers(Pop, Age, OAG = OAG)
+		if (any(out < 0)){
+			cat("Warning: beers produced negative values")
+		}
+		return(out)
+	}
+
+	if (plan == "agesmth(constrained)-sprague"){
+#		assess_in <- assess_pop(Pop, Age)
+		
+		out1 <- agesmth(Pop, Age, method = "Carrier-Farrag")
+		out  <- sprague(out1, Age = names2age(out1), OAG = OAG)
+		if (any(out < 0)){
+			cat("Warning: sprague produced negative values")
+		}
+		return(out)
+#		out2 <- agesmth(Pop, Age, method = "KKN")
+#		out3 <- agesmth(Pop, Age, method = "Zigzag")
+#		
+#		assess_out1 <- assess_pop(out1, names2age(out1))
+#		assess_out2 <- assess_pop(out2, names2age(out2))
+#		assess_out3 <- assess_pop(out3, names2age(out3))
+#		
+#		plot(names2age(out1), out1,type='o')
+#		lines(names2age(out1), out2,col="red")
+#		lines(names2age(out1), out3,col="blue")
+		# need ordinal shortcut... here zigzag does better?
+		# not really if you look at visual...
+	}
+	if (plan == "agesmth(aggressive)-sprague"){
+		out1 <- agesmth(Pop, Age, method = "Strong")
+		
+		# this will affect tails, even if tails not adjusted!
+		out2 <- rescale.vector(out1,scale = sum(Pop))
+		out  <- sprague(out1, Age = names2age(out2), OAG = OAG)
+		if (any(out < 0)){
+			cat("Warning: sprague produced negative values")
+		}
+		return(out)
+}
+}
+
+adjust_pop <- function(Pop, Age, OAG = TRUE, 
+		ageMin = 30, ageMax = min(c(70, max(Age[Age%%10 == 0]) - 10))){
+	assessment <- assess_pop(Pop, Age, ageMin = ageMin, ageMax = ageMax)
+	plan       <- plan_pop_adjustment(assessment)
+	if (plan == "visual assessment required"){
+		cat(plan,"\n")
+	}
+	adjust_pop_with_plan(Pop = Pop, Age = Age, plan = plan, OAG = OAG)
+}
+
+
+plot(Age,h1,pch=1,cex=.7)
+lines(Age,adjust_pop(h1,Age,FALSE),col="red")
+
+plot(Age,h2,pch=1,cex=.7)
+lines(Age,adjust_pop(h2,Age,FALSE),col="red")
+
+plot(Age,h3,pch=1,cex=.7)
+lines(Age,adjust_pop(h3,Age,FALSE),col="red")
+
+plot(Age,h4,pch=1,cex=.7)
+lines(Age,adjust_pop(h4,Age,FALSE),col="red")
+
+plot(Age,h5,pch=1,cex=.7)
+lines(Age,adjust_pop(h5,Age,FALSE),col="red")
+
+plot(Age,pop1m_pasex,pch=1,cex=.7)
+lines(Age,adjust_pop(pop1m_pasex,Age,FALSE),col="red")
+
+plot(0:100,pop1m_ind,pch=1,cex=.7)
+lines(0:100,adjust_pop(pop1m_ind,0:100,TRUE),col="red")
+
+# ----------------------------------------------------
+# below various pieces of scratch code used in testing.
+
+
+
+# if single age data don't trigger Myers or Whipple, then no need for 5-year checks.
+# If single age tripped, and roughness light and sawtooth negligible, then sprague enough
+# if "                 , and roughness moderate and sawtooth light then beers enough
+# if "                 , and roughness moderate and sawtooth mod then agesmth constrained
+# if "                 , and roughness >mod | sawtooth > mod then agesmth aggressive
+
+inda <- assess_pop(pop1m_ind,0:100)
+plan_pop_adjustment(inda)
+
+plot(0:100,pop1m_ind,type="o")
+lines(0:100, sprague(agesmth(pop1m_ind,0:100,method="Strong"),Age=seq(0,100,by=5),OAG=TRUE))
 # hmmmm. It would seem natural variation could easily bring Myers of .5 or Whipple of 1.03
 # maybe best not to do anything.
 # per UN Whipple < 1.05 very accurate. (Myers < .5)
 
 # Sprague or nothing if:
-(Whipple(h3,Age) < 1.05 | Myers(h3,Age) < 1)  &
-( five_year_roughness(h3,Age) < .1 | zero_pref_sawtooth(h3,Age) == 0 )
+(Whipple(h1,Age) < 1.05 | Myers(h1,Age) < 1)  &
+( five_year_roughness(h1,Age) < .1 | zero_pref_sawtooth(h1,Age) == 0 )
+h1a <- assess_pop(h1,Age)
+plan_pop_adjustment(h1a)
 
 # Beers if: not sprague... AND:
-(Whipple(h2,Age) < 1.25 | Myers(h2,Age) < 5)  &
-		( five_year_roughness(h2,Age) < .2 | zero_pref_sawtooth(h2,Age) < .05)
+((Whipple(h2, Age) < 1.2 & Whipple(h2, Age) >= 1.05) | 
+			(Myers(h2, Age) >= 1 & Myers(h2, Age) < 3)) &
+		( five_year_roughness(h2, Age) < .1 | zero_pref_sawtooth(h2,Age) < 1.1)
+h2a <- assess_pop(h2,Age)
+plan_pop_adjustment(h2a)
+
+# still didnt trigger beers
+h2.2 <- heapify(smoothed, Age, p0 = .21, p5 = .2, 20, 95)
+h2.2a <- assess_pop(h2.2,Age)
+plan_pop_adjustment(h2.2a)
 
 # agesmth(constrained)-sprague if:
-(Whipple(h1,Age) >= 1.25 | Myers(h1,Age) >= 5) &
-		( five_year_roughness(h1,Age) >= .2 | zero_pref_sawtooth(h1,Age) > 0 )
+((Whipple(h3,Age) >= 1.2 & Whipple(h3,Age) < 1.5) | 
+			(Myers(h3,Age) >= 3) & (Myers(h3,Age) < 10) & # same
+		( five_year_roughness(h3,Age) >= .1 & five_year_roughness(h3,Age) < .5 | 
+			(zero_pref_sawtooth(h3,Age) > 1.05 & zero_pref_sawtooth(h3,Age) < 1.5 ))
+h3a <- assess_pop(h3,Age)
+plan_pop_adjustment(h3a)
 
 # agesmth(strongly)-sprague if:
-(Whipple(h4,Age) >= 1.25 | Myers(h4,Age) >= 5) &
-		( five_year_roughness(h4,Age) >= .5 | zero_pref_sawtooth(h4,Age) > 1 )
+(Whipple(h4,Age) >= 1.5 | Myers(h4,Age) >= 10) &
+		( five_year_roughness(h4,Age) >= .5 | zero_pref_sawtooth(h4,Age) >= 1.5 )
+h4a <- assess_pop(h4,Age)
+plan_pop_adjustment(h4a)
 
+(Whipple(h5,Age) >= 1.5 | Myers(h5,Age) >= 10) & # same
+		( five_year_roughness(h5,Age) >= .5 | zero_pref_sawtooth(h5,Age) < 1.5 )
+h5a <- assess_pop(h5,Age)
+plan_pop_adjustment(h5a)
+
+plot(Age,smoothed,type='l')
+lines(Age,beers(h5,Age,OAG=FALSE),col="blue")
+lines(Age,sprague(agesmth(h5,Age,method="Carrier-Farrag"),Age=seq(0,95,by=5),OAG=FALSE),col="red")
 
 # which ones constrained?
 A5 <- seq(0,95,by=5)
