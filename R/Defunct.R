1bd700fe6819c895c80e2b6592e46793c36dc0bd
Tim Riffe <tim.riffe@gmail.com>
2019-10-07 11:41:06 +0200

    add defunct script for future deprecations

---
+++ R/Defunct.R
@@ -0,0 +1,17 @@
+# Function renaming coming soon.
+# This page will contain deprecated function names.
+# https://ropensci.org/technotes/2017/01/05/package-evolution/
+
+
+# Ideas: 
+# 1) snake case for method/ function families:
+# 2) first part denotes family, second function.
+# 3) so instead of LTabr() use lt_abridged()
+#    lt_single(). For identity functions. mx2qx -> lt_identity_mx_to_qx
+# These are more verbose names, but the longer ones are just internal,
+# using segmented regular names like this helps with autocomplete in RStudio.
+
+# 4) for the sake of autocomplete, carrier_farrag_smth(), should become smth_carrier_farrag()
+
+# 5) like wise, Sprague -> graduate_sprague, Beers -> graduate_beers()
+# 6) Other families: age_util_XXXX, 
