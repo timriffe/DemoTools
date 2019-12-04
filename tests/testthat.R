52c283931e6279d9dd70555eca7e4bf7158f0a6b
Tim Riffe <tim.riffe@gmail.com>
2019-12-04 10:58:30 +0100

    fix graduate_sprague example

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,6 +1,13 @@
-library(testthat)
-library(DemoTools)
-library(magrittr)
-library(dplyr)
+shhh <- function(expr){
+  capture.output(x <- suppressPackageStartupMessages(
+    suppressMessages(suppressWarnings(expr))))
+  invisible(x)
+}
+
+
+shhh(library(testthat))
+shhh(library(DemoTools))
+shhh(library(magrittr))
+shhh(library(dplyr))
 
 test_check("DemoTools")
