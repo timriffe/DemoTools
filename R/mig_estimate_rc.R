2addcf6a24e6edde054130328f9e117e28f13094
MJAlexander <monicaalexander@berkeley.edu>
2019-12-29 16:25:10 -0500

    fixd notes??

--- R/mig_estimate_rc.R
+++ R/mig_estimate_rc.R
@@ -13,8 +13,7 @@
 #' @param ... additional inputs to stan, see ?rstan::stan for details. 
 #' @importFrom rstan stan extract
 #' @importFrom stats quantile
-#' @importFrom dplyr group_by
-#' @importFrom dplyr summarise
+#' @importFrom dplyr group_by summarise rename
 #' @importFrom tidyr gather
 #' @importFrom rlang sym
 #' @export
@@ -66,20 +65,17 @@ mig_estimate_rc <- function(ages,
                  lower = apply(y_hat, 2, quantile,0.025),
                  upper = apply(y_hat, 2, quantile, 0.975),
                  diff_sq = (!!sym("median") - !!sym("data"))^2)
-
-  pars_df <- rc_fit %>% tidybayes::spread_draws(`a[0-9]`,
-                                                `alpha[0-9]`,
-                                                `mu[0-9]`,
-                                                `lambda[0-9]`,
-                                                `^c$`,
+  pars_df <- rc_fit %>% tidybayes::gather_draws(!!sym("a[0-9]"),
+                                                !!sym("alpha[0-9]"),
+                                                !!sym("mu[0-9]"),
+                                                !!sym("lambda[0-9]"),
+                                                !!sym("^c$"),
                                                 regex = TRUE) %>%
-    # TR: can we plz switch to pivot_longer and add @imporFrom dplyr pivot_longer ?
-    # I didn't switch this cuz I didn't have live data to test it on.
-    gather(!!sym("variable"), !!sym("value"), -!!sym(".chain"), -!!sym(".iteration"), -!!sym(".draw")) %>%
-    group_by(!!sym("variable")) %>%
-    summarise(median = median(!!sym("value")),
-              lower = quantile(!!sym("value"), 0.025),
-              upper = quantile(!!sym("value"), 0.975))
+    group_by(!!sym(".variable")) %>%
+    summarise(median = median(!!sym(".value")),
+              lower = quantile(!!sym(".value"), 0.025),
+              upper = quantile(!!sym(".value"), 0.975)) %>% 
+    dplyr::rename("variable" = !!sym(".variable"))
   
   return(list(pars_df = pars_df, fit_df = dfit))
 }
