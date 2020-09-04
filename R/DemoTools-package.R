5cd0120313e22f10bd7b5a5ca27593af1d1bb4ae
MJAlexander <monicaalexander@berkeley.edu>
2020-09-04 06:54:37 -0400

    reverting to old version of stan code

--- R/DemoTools-package.R
+++ R/DemoTools-package.R
@@ -3,10 +3,7 @@
 # "_PACKAGE" imports the details from DESCRIPTION
 
 #' @import demogR Rdpack
-#' @import methods
 #' @import Rcpp
-#' @importFrom rstan sampling
-#' @useDynLib DemoTools, .registration=TRUE
 #' @importFrom MortalityLaws MortalityLaw
 #' @importFrom stats coef predict
 #' @name DemoTools-package
