1ea5f1ea57cf4a08387484a780190642860a640a
Tim Riffe <tim.riffe@gmail.com>
2020-10-31 09:49:36 +0100

    simple cohort interp added

--- R/interp_coh.R
+++ R/interp_coh.R
@@ -2,7 +2,7 @@
 #' @description Matches the (single) ages of a census to single cohorts. For use in intercensal interpolations. Ages are potentially blended to match single cohort line assuming that the population in each age is uniformly distributed over the age group.
 #' @param Age integer. Lower bound of single age groups
 #' @param date Either a \code{Date} class object or an unambiguous character string in the format \code{"YYYY-MM-DD"}.
-
+#'@export
 #' @examples
 #' Pop <- seq(10000,100,length.out = 101)
 #' Age <- 0:100
@@ -49,8 +49,18 @@ census_cohort_adjust <- function(Pop, Age, date){
 # 
 # interp()
 
-c1 = seq(10000,10,length.out = 10); c2 = seq(15000,10,length.out = 10); date1 = "2020-07-01"; date2 = "2025-10-14"; age1 = 0:9; age2 = 0:9
+# c1 = seq(10000,10,length.out = 10); c2 = seq(15000,10,length.out = 10); date1 = "2020-07-01"; date2 = "2025-10-14"; age1 = 0:9; age2 = 0:9
 
+#' component-free intercensalcohort interpolation
+#' @description Cohorts between two censuses are interpolated flexibly using linear, exponential, or power rules. The lower and upper intercensal triangles are filled using within-age interpolation. This function is experimental and still in development.
+#' @seealso interp
+#' @param c1 numeric vector. The first (left) census in single age groups
+#' @param c1 numeric vector. The second (right) census in single age groups
+#' @param date1 reference date of c1`. Either a Date class object or an unambiguous character string in the format "YYYY-MM-DD".
+#' @param date2 reference date of c2`. Either a Date class object or an unambiguous character string in the format "YYYY-MM-DD".
+#' @param age1 integer vector. single ages of `c1`
+#' @param age2 integer vector. single ages of `c2` 
+#' @export
 interp_coh_bare <- function(c1, c2, date1, date2, age1, age2, ...){
   
   date1 <- dec.date(date1)
@@ -149,19 +159,19 @@ interp_coh_bare <- function(c1, c2, date1, date2, age1, age2, ...){
 
 
 # try out
-
-boo <- interpolated_period_mat
-
-boo[is.numeric(boo)] <- 0
-
-foo <- interpolated_coh_mat
-
-
-
-for (i in dimnames(foo)[[2]]) {
-  
-  replacement <- foo[,i]
-  ages <- as.numeric(i) - as.numeric(names(replacement))
-  
-  boo[ages,i] <- replacement
-}
+# 
+# boo <- interpolated_period_mat
+# 
+# boo[is.numeric(boo)] <- 0
+# 
+# foo <- interpolated_coh_mat
+# 
+# 
+# 
+# for (i in dimnames(foo)[[2]]) {
+#   
+#   replacement <- foo[,i]
+#   ages <- as.numeric(i) - as.numeric(names(replacement))
+#   
+#   boo[ages,i] <- replacement
+# }
