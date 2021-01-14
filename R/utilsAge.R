a06e1947ac3b82ec8bc9ddf0d413aadfa8827d32
Tim Riffe <tim.riffe@gmail.com>
2021-01-14 16:20:48 +0100

    tend to documentation of rescaleAgeGroups()

--- R/utilsAge.R
+++ R/utilsAge.R
@@ -568,16 +568,16 @@ names2age <- function(...) {
 #' @description This method rescales a vector of counts in arbitrary (integer) age groups to approximate a vector of counts in a potentially different age grouping. Common use cases will be to scale single ages (whose age pattern we wish to roughly maintain) to sum to abridged or 5-year age groups from another source. The counts to be rescaled could potentially be in any grouping (see example).
 #' @details If the final age group is open, define its age interval as 1.
 #'
-#' Presently the intermediate splitting function can either be \code{graduate_uniform()} or \code{graduate_mono()}.
+#' Presently the intermediate splitting function can either be `graduate_uniform()` or `graduate_mono()`.
 #'
-#' The method is an original contribution. It works by first splitting the counts of \code{Value1} to single ages using the assumptions of \code{splitfun()}. \code{Value1} is then rescaled such that were it re-grouped to match the age classes of \code{Value2} they would be identical. If \code{recursive = FALSE}, the single-age rescaled \code{Value1} data are returned regrouped to their original ages. If \code{recursive = TRUE}, the process is repeated until \code{Value1} is rescaled such that it could be split and regrouped to \code{Value2} using the same process a single time with no need for further rescaling. If age groups in \code{Value1} are very irregular, \code{recursive = TRUE} can induce noise (see example). If the age groups of \code{Value1} nest cleanly within the age groups of \code{Value2} then recursion is unnecessary. This is the case, for example, whenever \code{Value1} is in single ages and \code{Value2} is in grouped ages, which is likely the most common usage scenario.
+#' The method is an original contribution. It works by first splitting the counts of `Value1` to single ages using the assumptions of `splitfun()`. `Value1` is then rescaled such that were it re-grouped to match the age classes of `Value2` they would be identical. If `recursive = FALSE`, the single-age rescaled `Value1` data are returned regrouped to their original ages. If `recursive = TRUE`, the process is repeated until `Value1` is rescaled such that it could be split and regrouped to `Value2` using the same process a single time with no need for further rescaling. If age groups in `Value1` are very irregular, `recursive = TRUE` can induce noise (see example). If the age groups of `Value1` nest cleanly within the age groups of `Value2` then recursion is unnecessary. This is the case, for example, whenever `Value1` is in single ages and `Value2` is in grouped ages, which is likely the most common usage scenario.
 #' @param Value1 numeric vector. A vector of demographic counts for population 1.
 #' @param AgeInt1 integer vector. Age interval widths for population 1.
 #' @param Value2 numeric vector. A vector of demographic counts for population 2.
 #' @param AgeInt2 integer vector. Age interval widths for population 2.
-#' @param splitfun function to use for splitting \code{Value1}. Presently only \code{graduate_uniform()} works.
-#' @param recursive logical. Shall we repeat the split/regroup/rescale process until stable? See details. Default \code{FALSE}.
-#' @param tol numeric. Default 1e-3. The numerical tolerance for the residual. Used to detect stability if \code{recursive = TRUE}.
+#' @param splitfun function to use for splitting `Value1`. Reasonable (and tested) choices are either `graduate_uniform` or `graduate_mono`.
+#' @param recursive logical. Shall we repeat the split/regroup/rescale process until stable? See details. Default `FALSE`.
+#' @param tol numeric. Default `1e-3`. The numerical tolerance for the residual. Used to detect stability if `recursive = TRUE`.
 #' @export
 #'
 #' @examples
