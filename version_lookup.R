67a40f2bb298877a07054f92aea3be3e33458e56
Tim Riffe <tim.riffe@gmail.com>
2021-06-09 22:57:17 +0200

    fixes install_DemoTools_version()

--- version_lookup.R
+++ version_lookup.R
@@ -11,30 +11,36 @@ if (system("whoami",intern=TRUE) == "tim"){
       gsub(pattern = "Version: ", replacement = "") 
   }
   
+  update_lookup <- function(){
+    DESC_changes <- 
+      parse_log_detailed() %>% 
+      unnest_log() %>% 
+      dplyr::filter(changed_file == "DESCRIPTION") %>% 
+      group_by(lubridate::as_date(date)) %>% 
+      slice(n()) %>% 
+      ungroup() %>% 
+      mutate(date = as_date(date))
+        #date = paste(year(date),month(date),day(date),sep="-"))
+    # DESC_changes %>% 
+    #   mutate(get_versions(hash))
+    vers <- list()
+    for (i in 1:nrow(DESC_changes)){
+      vers[[i]] <- try(get_versions(DESC_changes$hash[i]))
+    }
+    closeAllConnections()
+    errors <- lapply(vers,class) %>% unlist() 
+    errors <- errors == "try-error"
+    vers[errors]<-NA
+    DESC_changes$version <- vers
+    
+    version_lookup <- 
+      DESC_changes %>% 
+      filter(!is.na(version)) %>% 
+      select(date,version, hash)
+    
+    write_csv(version_lookup,"version_lookup.csv")
   
-  DESC_changes <- 
-    parse_log_detailed() %>% 
-    unnest_log() %>% 
-    dplyr::filter(changed_file == "DESCRIPTION") %>% 
-    group_by(lubridate::as_date(date)) %>% 
-    slice(n()) %>% 
-    ungroup() %>% 
-    mutate(date = as_date(date))
-      #date = paste(year(date),month(date),day(date),sep="-"))
-  # DESC_changes %>% 
-  #   mutate(get_versions(hash))
-  vers <- list()
-  for (i in 1:nrow(DESC_changes)){
-    vers[[i]] <- try(get_versions(DESC_changes$hash[i]))
-  }
-  DESC_changes$version <- unlist(vers)
-  
-  version_lookup <- 
-    DESC_changes %>% 
-    select(date,version, hash)
-  
-  write_csv(version_lookup,"version_lookup.csv")
-  
+   }
 }
 
 get_DemoTools_versions <- function(){
@@ -42,6 +48,7 @@ get_DemoTools_versions <- function(){
 }
 
 install_DemoTools_version <- function(version = NULL, date = NULL, hash = NULL){
+  
   
   
   if (is.null(version) & is.null(date) & is.null(hash)){
@@ -55,7 +62,8 @@ install_DemoTools_version <- function(version = NULL, date = NULL, hash = NULL){
         remotes::install_github('timriffe/DemoTools'")
     }
   
-  versions <- get_DemoTools_versions()
+  versions <- get_DemoTools_versions() %>% 
+    mutate(date = lubridate::as_date(date))
   
   out <- NULL
   if (!is.null(hash)){
@@ -70,10 +78,18 @@ install_DemoTools_version <- function(version = NULL, date = NULL, hash = NULL){
   # try date
   if (is.null(out)){
     if (!is.null(date)){
+      if (is.character(date)){
+        date = lubridate::ymd(date)
+      }
+      if (is.na(date)){
+        cat("date didn't parse. It should be yyyy-mm-dd format if given as character\n")
+        stop()
+      }
+      dateK <- date
         dateL <- 
           versions %>% 
-          mutate(dist = abs(date - date)) %>% 
-          dplyr::filter(dist = min(dist)) %>% 
+          mutate(dist = abs(dateK - date)) %>% 
+          dplyr::filter(dist == min(dist)) %>% 
           dplyr::pull(date) %>% 
           '['(1)
         hash <- 
@@ -84,7 +100,7 @@ install_DemoTools_version <- function(version = NULL, date = NULL, hash = NULL){
         out <- 1
         if (date != dateL){
           cat("date not in the (incomplete) set returned by get_DemoTools_versions()
-              using the closest date in that subset instead:", dateL,"\n")
+              using the closest date in that subset instead:", as.character(dateL),"\n")
         }
       }
   }
@@ -92,9 +108,10 @@ install_DemoTools_version <- function(version = NULL, date = NULL, hash = NULL){
   # try version
   if (is.null(out)){
     if (!is.null(version)){
+      .version <- version
       hash <- 
         versions %>% 
-        dplyr::filter(version == version) %>% 
+        dplyr::filter(version == .version) %>% 
         dplyr::pull(hash)
       if (length(hash) == 1){
         remotes::install_github("timriffe/DemoTools", ref = hash)  
