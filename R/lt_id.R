36893cde221916fd7d06fa48a97110a976b739e1
Ivan Williams <ivanwilliams1985@gmail.com>
2021-08-19 11:36:01 -0300

    update condition on Lx

--- R/lt_id.R
+++ R/lt_id.R
@@ -283,6 +283,8 @@ lt_id_ma_q <- function(nMx, nax, AgeInt, closeout = TRUE, IMR) {
 lt_id_Ll_S      <- function(nLx, lx = NULL, Age, AgeInt = NULL, N = 5) {
   # number ages
   n               <- length(nLx)
+  # same length of vectors
+  stopifnot(length(nLx) == length(Age))
   # either we're in 1 or 5 year age groups
   stopifnot(length(N) == 1 & N %in% c(5, 1))
   # infer radix in case lx is not given
@@ -290,8 +292,8 @@ lt_id_Ll_S      <- function(nLx, lx = NULL, Age, AgeInt = NULL, N = 5) {
   if(is.null(lx)){
     radix <- ifelse(nLx[1]>1, 10^nchar(trunc(nLx[1])), 1)  
   }
-  # validate nLx
-  stopifnot(all(nLx>=0, nLx[-n] < (radix*N)))
+  # validate nLx. Some zero in ages>100 could happen
+  stopifnot(all(nLx>=0, nLx[Age<100]>0, nLx[-n] < (radix*N)))
   
   ## compute Sx (missing from the LTbr computation)
   # first age group is survival from births to the second age group
